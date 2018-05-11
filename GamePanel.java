504ed70aa9f1c59ad565915d3888849c8cee9fc3
Nolan Origer <nolanoriger@gmail.com>
2018-05-11 14:04:51 -0700

    platform * 72 and platform locations

--- GamePanel.java
+++ GamePanel.java
@@ -26,6 +26,30 @@ public class GamePanel extends MyPanel{
         gameObjects.add(new Platform(this,21,1,8,1));
         gameObjects.add(new Platform(this,27,0,2,1));
         gameObjects.add(new Platform(this,42,0,1,18));
+        gameObjects.add(new Platform(this,3,10,38,1));
+        gameObjects.add(new Platform(this,13,7,1,3));
+        gameObjects.add(new Platform(this,14,8,1,2));
+        gameObjects.add(new Platform(this,15,9,1,1));
+        gameObjects.add(new Platform(this,19,7,4,1));
+        gameObjects.add(new Platform(this,28,9,1,1));
+        gameObjects.add(new Platform(this,29,8,1,2));
+        gameObjects.add(new Platform(this,30,7,1,3));
+        gameObjects.add(new Platform(this,33,7,1,3));
+        gameObjects.add(new Platform(this,34,8,1,2));
+        gameObjects.add(new Platform(this,35,9,1,1));
+        gameObjects.add(new Platform(this,4,15,1,2));
+        gameObjects.add(new Platform(this,5,16,1,1));
+        gameObjects.add(new Platform(this,8,16,1,1));
+        gameObjects.add(new Platform(this,9,15,1,2));
+        gameObjects.add(new Platform(this,1,17,40,1));
+        gameObjects.add(new Platform(this,12,13,2,1));
+        gameObjects.add(new Platform(this,16,15,1,2));
+        gameObjects.add(new Platform(this,17,16,1,1));
+        gameObjects.add(new Platform(this,21,16,1,1));
+        gameObjects.add(new Platform(this,23,15,1,2));
+        gameObjects.add(new Platform(this,25,14,1,3));
+        gameObjects.add(new Platform(this,27,15,1,2));
+        gameObjects.add(new Platform(this,29,16,1,1));
         gameObjects.add(new Minigame(this,700,100,100,100,MinigamePanel.class));
     }
     public void paintComponent(Graphics g){
