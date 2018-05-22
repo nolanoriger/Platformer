794545f3d930aecc40fc144715870b13b776318f
EthanSamuels <38737425+EthanSamuels@users.noreply.github.com>
2018-05-22 12:34:12 -0700

    Add files via upload

--- ParLostLevel.java
+++ ParLostLevel.java
@@ -4,6 +4,18 @@ public class ParLostLevel extends GamePanel{
         super(gc);
         //Construct the level
         ArrayList<GameObject> gameObjects = getGameObjects();
+        gameObjects.add(new Platform(this,0,3,7,10));
+        gameObjects.add(new Platform(this,7,2,4,10));
+        gameObjects.add(new Platform(this,11,4,11,10));
+        gameObjects.add(new Platform(this,16,0,3,1));
+        gameObjects.add(new Platform(this,22,3,2,10));
+        gameObjects.add(new Platform(this,24,2,2,10));
+        gameObjects.add(new Platform(this,26,1,5,10));
+        gameObjects.add(new Platform(this,30,0,3,10));
+        gameObjects.add(new Platform(this,36,-1,3,1));
+        gameObjects.add(new Platform(this,33,5,10,10));
+        
+        /*
         gameObjects.add(new Platform(this,0,0,1,18));
         gameObjects.add(new Platform(this,1,2,10,1));
         gameObjects.add(new Platform(this,7,1,4,1));
@@ -14,7 +26,7 @@ public class ParLostLevel extends GamePanel{
         gameObjects.add(new Platform(this,19,2,10,1));
         gameObjects.add(new Platform(this,21,1,8,1));
         gameObjects.add(new Platform(this,27,0,2,1));
-        gameObjects.add(new Platform(this,42,0,1,18));
+        gameObjects.add(new Platform(this,42,0,2,1));
         gameObjects.add(new Platform(this,3,10,38,1));
         gameObjects.add(new Platform(this,13,7,1,3));
         gameObjects.add(new Platform(this,14,8,1,2));
@@ -40,5 +52,6 @@ public class ParLostLevel extends GamePanel{
         gameObjects.add(new Platform(this,27,15,1,2));
         gameObjects.add(new Platform(this,29,16,1,1));
         gameObjects.add(new Minigame(this,0,0,100,100,FruitCatchMinigame.class));
+        */
     }
 }
